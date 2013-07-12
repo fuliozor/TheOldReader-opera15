5582e2eb26027db85c9fa6d5dfc0a52113e4a554
Fuliozor <alexandr.khomenock@gmail.com>
2013-07-12 17:25:56 +0300

    init

---
+++ sources/source.js
@@ -0,0 +1,22 @@
+/**
+ * Класс отвечающий за работу с UI
+ * @returns {undefined}
+ */
+function UI() {
+	
+}
+
+/**
+ * Класс отвечающий за работу с сетью
+ * @returns {undefined}
+ */
+function Proxy() {
+	
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function Controller() {
+}
