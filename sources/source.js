7086a60ea5bc9dc4fbeea5258b7142782bdb4a6b
Fuliozor <alexandr.khomenock@gmail.com>
2013-07-12 18:02:35 +0300

    MS

--- sources/source.js
+++ sources/source.js
@@ -10,7 +10,33 @@ function UI() {
  * Класс отвечающий за работу с сетью
  * @returns {undefined}
  */
-function Proxy() {
+function Proxy() {	
+	this.headers;
+	this.xmlhttp = new XMLHttpRequest();;
+}
+
+/**
+ * Создает XMLHttpRequest с задаными параметрами
+ * @param {string} url - урл
+ * @returns {XMLHttpRequest}
+ */
+Proxy.prototype.createRequest = function(url, callbackSuccess, callbackError) {
+	this.xmlhttp.open('POST', url, callback);
+	this.xmlhttp.onreadystatechange = function() {
+		if(this.xmlhttp.readyState == 4) {
+			if(this.xmlhttp.status == 200) {
+				callbackSuccess(xmlhttp.responseText, xmlhttp);
+			} else {
+				callbackError(xmlhttp.responseText, xmlhttp)
+			}
+		}
+		
+	};
+	
+	return this.xmlhttp.open('POST', url, callback);
+}
+
+Proxy.prototype.addHeader = function(name, value) {
 	
 }
 
